4443d2a40a5e7323e0dbb0eedb851fec222b9d00
Julian Berman <Julian@GrayVines.com>
2016-09-07 07:46:40 -0400

    And the nonexistant.

--- filesystems/tests/test_native.py
+++ filesystems/tests/test_native.py
@@ -23,3 +23,15 @@ class TestNative(TestCase):
             ),
             dict(exists=True, is_dir=True, is_file=False, is_link=False),
         )
+
+    def test_nonexistant(self):
+        nonexistant = Path.from_string(__file__).descendant("asdf")
+        self.assertEqual(
+            dict(
+                exists=self.fs.exists(path=nonexistant),
+                is_dir=self.fs.is_dir(path=nonexistant),
+                is_file=self.fs.is_file(path=nonexistant),
+                is_link=self.fs.is_link(path=nonexistant),
+            ),
+            dict(exists=False, is_dir=False, is_file=False, is_link=False),
+        )
