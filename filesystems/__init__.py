5a6a86f8abf7b0321109b200b24846f65df6a52c
Julian Berman <Julian@GrayVines.com>
2019-01-01 09:40:38 +0000

    Go away.

--- filesystems/__init__.py
+++ filesystems/__init__.py
@@ -4,7 +4,7 @@ _PY3 = sys.version_info[0] >= 3
 from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     pass
 
 from filesystems._path import Path
