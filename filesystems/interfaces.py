a0a2930f870450e501079aae188a2dc7119590bd
Julian Berman <Julian@GrayVines.com>
2019-01-06 17:08:58 +0000

    z.i for Paths, and flesh out the rest of RelativePath.

---
+++ filesystems/interfaces.py
@@ -0,0 +1,61 @@
+from zope.interface import Interface
+
+from filesystems import _PY3
+
+
+class Path(Interface):
+    def __str__():
+        """
+        Render the path as a string.
+        """
+
+    if _PY3:
+        def __truediv__(other):
+            """
+            Traverse to a child of this path.
+            """
+
+        def __fspath__(other):
+            """
+            Render the path as a string.
+            """
+    else:
+        def __div__(other):
+            """
+            Render the path as a string.
+            """
+
+    def basename():
+        """
+        The tail component of this path.
+        """
+
+    def dirname():
+        """
+        The head components of this path.
+        """
+
+    def heritage():
+        """
+        The top-down set of this path's parents.
+        """
+
+    def descendant(*segments):
+        """
+        Traverse to a descendant of this path.
+        """
+
+    def parent():
+        """
+        Traverse to the parent of this path.
+        """
+
+    def sibling(name):
+        """
+        Traverse to a sibling of this path.
+        """
+
+    def relative_to(path):
+        """
+        Resolve a path relative to this one.
+        """
